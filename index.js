d97e9d1c5e56d679ff1b89801d264ece7885ae37
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2023-05-06 02:30:33 +0100

    portfolio task

---
+++ index.js
@@ -0,0 +1,9 @@
+$('#moonLight').on('click',()=>{
+    $('body').toggleClass('dark');
+    $('.bottom-container').toggleClass('dark');
+    $('.top_container').toggleClass('dark');
+    $('.test').toggleClass('textColor');
+    
+    
+    
+})
